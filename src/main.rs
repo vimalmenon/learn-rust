c42c319a9a627d128513e98494a70b758f0f2da6
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-06-02 11:19:37 +0800

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main()  {
+    println!("Hello, world!");
+    let another_str = "test";
+    let mut string:String = String::from("Vimal Menon");
+    let new_string  = String::new();
+    let number :u8= 5;
+    let value: u8 = add(&number);
+    println!("{value} {} {} {} {new_string}", string, number, another_str);
+
+    string.push_str("Value");
+}
+
+fn add(number:&u8) -> u8 {
+    println!("Adding number {:p}",number);
+    return number + 5;
+}
+

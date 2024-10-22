6a37e125fc95ee4ff8a9099ced1c08bd9668d92e
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-10-22 08:27:51 +0800

    updated README

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ fn main() {
 ```
 
 
-#### Example of Borrowed
+#### Example of Borrowing
 ```rs
 fn main() {
     let mut string = String::from("This is string");
@@ -55,4 +55,39 @@ fn borrowed(string: &mut String) {
     println!("This is Inside, {}", string);
 }
 ```
+##### Example for Loop
+```rs
+fn main() {
+    
+    let mut items = [String::from("One"), String::from("Two"), String::from("Three")];
+    simple_loop(&mut items);
+    for_loop(&mut items);
+    while_loop(&mut items);
+}
+fn simple_loop(items: &mut [String; 3]) {
+    let mut counter = 0;
+    loop {
+        if counter == items.len() {
+            break;
+        }
+        items[counter].push_str(" - simple loop");
+        println!("Simple Loop : {}", items[counter]);
+        counter += 1;
+    }
+}
+fn for_loop(items: &mut [String; 3]) {
+    for item in items.iter_mut() {
+        item.push_str(" - for loop");
+        println!("For Loop: {}", item);
+    }
+}
 
+fn while_loop(items: &mut [String; 3]) {
+    let mut counter = 0;
+    while counter < items.len() {
+        items[counter].push_str(" - while loop");
+        println!("While Loop: {}", items[counter]);
+        counter += 1;
+    }
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 fn main() {
-    let mut string = String::from("This is string");
-    println!("This is Before, {}", string);
-    borrowed(&mut string);
-    string.push_str(", This is after borrowed");
-    println!("This is After, {}", string);
+    
+    let mut items = [String::from("One"), String::from("Two"), String::from("Three")];
+    simple_loop(&mut items);
+    for_loop(&mut items);
+    while_loop(&mut items);
 }
 
-fn borrowed(string: &mut String) {
-    string.push_str(", This is inside borrowed");
-    println!("This is Inside, {}", string);
+fn simple_loop(items: &mut [String; 3]) {
+    let mut counter = 0;
+    loop {
+        if counter == items.len() {
+            break;
+        }
+        items[counter].push_str(" - simple loop");
+        println!("Simple Loop : {}", items[counter]);
+        counter += 1;
+    }
+}
+fn for_loop(items: &mut [String; 3]) {
+    for item in items.iter_mut() {
+        item.push_str(" - for loop");
+        println!("For Loop: {}", item);
+    }
+}
+
+fn while_loop(items: &mut [String; 3]) {
+    let mut counter = 0;
+    while counter < items.len() {
+        items[counter].push_str(" - while loop");
+        println!("While Loop: {}", items[counter]);
+        counter += 1;
+    }
 }

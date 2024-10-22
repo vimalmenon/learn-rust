ab9e6aa8c131d8f83729c81af5e1927ff3b5e6e5
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-10-23 05:52:38 +0800

    updated README

--- README.md
+++ README.md
@@ -119,3 +119,16 @@ fn string_fn() {
     */
 }
 ```
+```rs
+fn main() {
+    let some_str: &str = "vimal";
+    let another_str = some_str;
+    println!("{}, {}", some_str, another_str);
+    let some_number:u8 = 200;
+    let another_number = some_number;
+    println!("{}, {}", some_number, another_number);
+    // let some_string: String = String::from("Vimal");
+    // let another_string = some_string;
+    // println!("{}, {}", some_string, another_string);
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,12 @@
+
 fn main() {
-   str_fn();
-   string_fn();
-}
-
-fn str_fn() {
-    let test: &str = "Vimal";
-    println!("test : {}", test);
-    let test1 = test.to_string();
-    println!("test1 : {}", test1);
-    let test2 = test;
-    println!("test : {}, test1 : {}, test2 : {}",test, test1, test2);
-    let test3 = test1.clone();
-    println!("test : {}, test1 : {}, test2 : {}, test3 : {}",test, test1, test2, test3);
-}
-
-fn string_fn() {
-    let test = String::from("Vimal");
-    println!("test : {}", test);
-    let test1 = test.clone();
-    println!("test : {}, test1 : {}",test, test1);
-    let test2 = test.to_owned();
-    println!("test : {}, test1 : {}, test2 : {}",test, test1, test2);
-    /* 
-    This will failed because test1 is moved to test2
-    let test3 = test1;
-    println!("test : {}, test1 : {}, test2 : {}, test3 : {}",test, test1, test2, test3);
-    */
-
+    let some_str: &str = "vimal";
+    let another_str = some_str;
+    println!("{}, {}", some_str, another_str);
+    let some_number:u8 = 200;
+    let another_number = some_number;
+    println!("{}, {}", some_number, another_number);
+    // let some_string: String = String::from("Vimal");
+    // let another_string = some_string;
+    // println!("{}, {}", some_string, another_string);
 }

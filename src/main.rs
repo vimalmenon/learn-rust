42824a31eef53ebae04d11aa3bfd3577311e9b41
Vimal Menon <vimal.webs@gmail.com>
2026-08-20 09:31:31 +0800

    updated main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
+
 fn main() -> () {
     let _num: i64 = 563456000000;
     println!("Max is {}  Min is {}", i64::MAX, i64::MIN);
-    immutability();
-    string();
+    if false {
+        immutability();
+        string();
+        tuple();
+        destructuring();   
+    }
+}
+
+fn destructuring() {
+    let (first, second, ..) = (1, 2, "another", "value");
+    println!("This is value {} {} ", first, second);
+ 
+    let array = [1, 2, 3, 4];
+    let [first, second, rest @ ..] = array;
+    assert_eq!(first, 1);
+    assert_eq!(second, 2);
+    assert_eq!(rest, [3, 4]); 
+    println!("This is value {:?}", rest);
 }
 
 
+fn tuple() {
+    let numbers = (1, 2, "test", "empty");
+    // {:?} is debug trait
+    println!("This is tuple {:?}", numbers);
+    println!("This is tuple {}",  numbers.0);
+}
 
 // ## this code will fail because of immutability
 // fn fail_immutability() {

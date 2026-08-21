72d77bc202637f405e957f7c7e6bbd84aef36fad
Vimal Menon <vimal.webs@gmail.com>
2026-08-21 15:24:26 +0800

    updated text

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,19 @@
-#[derive(Debug)]
+use std::{fs::File, io::Write};
+
 #[warn(unused)]
 #[warn(dead_code)]
 
-struct Person {
-    name: String,
-    age: u8,
 
-}
-
-impl Person {
-    fn new(name: String, age: u8) -> Self {
-        Self { name, age }
-    }
-
-    fn say_name(&self) -> Option<&str>{
-        println!("My name is {}", self.name);
-        return None;
-    }
-    
-}
-
-
-struct Example {
-    name: String,
-}
-
-
-trait ExampleTrait {
-    fn new(name: String) -> Self;
-    fn say_name(&self) -> Option<&str>;
-    
-}
-
-impl ExampleTrait for Example {
-    fn new(name: String) -> Self {
-        Self { name }
-    }
-    fn say_name(&self) -> Option<&str> {
-        println!("My name is {}", self.name);
-        return None;
-    }    
-}
 
 fn main() -> () {
-    let example = Example::new(String::from("Bob"));
-    example.say_name();
+    let file = File::create("text.csv");
+    match file {
+        Ok(mut file) => {
+            file.write(b"1, 'Vimal Menon'").unwrap();
+            file.write(b"\n2, 'John Doe'").unwrap();
+        },
+        Err(e) => println!("Error creating file: {}", e),
+    }
 }
 
 

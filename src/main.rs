0911ed2d49ae80e1a5d6e710499aa61342a4c458
Vimal Menon <vimal.webs@gmail.com>
2026-08-21 14:58:54 +0800

    add Person struct and implement ExampleTrait; update main function to create and use Example instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() -> () {
-    println!("this is read");
+#[derive(Debug)]
+#[warn(unused)]
+#[warn(dead_code)]
+
+struct Person {
+    name: String,
+    age: u8,
+
 }
+
+impl Person {
+    fn new(name: String, age: u8) -> Self {
+        Self { name, age }
+    }
+
+    fn say_name(&self) -> Option<&str>{
+        println!("My name is {}", self.name);
+        return None;
+    }
+    
+}
+
+
+struct Example {
+    name: String,
+}
+
+
+trait ExampleTrait {
+    fn new(name: String) -> Self;
+    fn say_name(&self) -> Option<&str>;
+    
+}
+
+impl ExampleTrait for Example {
+    fn new(name: String) -> Self {
+        Self { name }
+    }
+    fn say_name(&self) -> Option<&str> {
+        println!("My name is {}", self.name);
+        return None;
+    }    
+}
+
+fn main() -> () {
+    let example = Example::new(String::from("Bob"));
+    example.say_name();
+}
+
+

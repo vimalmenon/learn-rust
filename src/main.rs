30fff2f1dc48a4fb674419da1fa8bdf313b3cd9d
Vimal Menon <vimal.webs@gmail.com>
2026-08-22 07:47:07 +0800

    updated README

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,28 @@ use std::{fs::File, io::Write};
 
 #[allow(unused, dead_code)]
 
+struct Person {
+    name: String,
+    age: u32,
+}
+
+trait PersonTrait {
+    fn new(name: String, age: u32) ->Person;
+    fn get_name(&self) -> &str;
+    fn get_age(&self) -> u32;
+}
+
+impl PersonTrait for Person {
+    fn new(name: String, age: u32) -> Self {
+        Self { name, age }
+    }
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+    fn get_age(&self) -> u32 {
+        self.age
+    }
+}
 
 
 fn main() -> () {
@@ -19,5 +41,9 @@ fn main() -> () {
     let doubled_items: Vec<i32> = items.iter().map(|x: &i32| x * 2).collect();
     println!("{:?}", doubled_items);
     println!("{:?}", items);
-}
 
+    let person = Person::new("Vimal Menon".to_string(), 25);
+    person.get_name();
+    person.get_age();
+
+}
